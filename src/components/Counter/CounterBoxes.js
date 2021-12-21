8a7912c354992cb025c55f69dca3dbf530b01d0c
Tfauves <tfauvelle@gmail.com>
2021-12-21 16:24:57 -0500

    collection display.

---
+++ src/components/Counter/CounterBoxes.js
@@ -0,0 +1,28 @@
+import React from "react";
+import CounterBox from "./CounterBox";
+
+const CounterBoxes = (props) => {
+    const displayBoxes = () => {
+        const output = [];
+        for (let count = 0; count < props.count; count++) {
+        output.push(
+            <CounterBox count={count} key={count} />
+        )
+    }
+    return output;
+
+    }
+
+    return (
+        <div style={{
+        display: 'flex',
+        flexDirection: "row",
+        flex: 1,
+        flexWrap: 'wrap'
+    }}>
+    {displayBoxes()}
+    </div>
+    )
+}    
+
+export default CounterBoxes;
