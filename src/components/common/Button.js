0d974b630ad94683b34c0e5ef6cb069269dded20
Tfauves <tfauvelle@gmail.com>
2021-12-21 15:41:08 -0500

    button

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -4,29 +4,32 @@ const Button = (props) => {
 
     return (
         <button
-        style={{
-            alignSelf: 'center',
-            backgroundColor: '#3343f2',
-            marginLeft: 5,
-            marginRight: 5,
-            marginBottom: 10,
-            marginTop: "50%",
-            borderRadius: 10,
-            fontSize: 16,
-            paddingTop: 5,
-            paddingBottom: 5,
-            fontWeight: "bold",
-            fontFamily: "sans-serif"
-        }}
+        style={{...styles.button, ...props.style}}
+        onClick={props.onClick}
+        disabled={props.disabled}
         >
-            {props.children}
+            {props.disabled ? (
+                <p>Loading...</p>
+            ) : (
+                props.children
+            )}
         </button>
-
     )
 }
-
 const styles = {
     button: {
+        alignSelf: 'center',
+        backgroundColor: '#3343f2',
+        marginLeft: 5,
+        marginRight: 5,
+        marginBottom: 10,
+        marginTop: "50%",
+        borderRadius: 10,
+        fontSize: 16,
+        paddingTop: 5,
+        paddingBottom: 5,
+        fontWeight: "bold",
+        fontFamily: "sans-serif"
 
     }
 }
