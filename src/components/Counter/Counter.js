7cb3180755b63b884451aef33e803d3cba593f1d
Tfauves <tfauvelle@gmail.com>
2021-12-22 10:57:25 -0500

    auth and home.

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import axios from 'axios';
 
 const Counter = props => {
-const num = 1;
-return (
+  const num = 1;
+
+  return (
     <div>
-     <p> This is a box {props.count}</p>   
+      <p>This is box {props.count}</p>
     </div>
-)
+  )
 }
 
 export default Counter;
