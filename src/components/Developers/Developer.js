9bf5dd17c2c42ac024faa3436b1dc6c30cd238bb
Tfauves <tfauvelle@gmail.com>
2021-12-23 19:58:28 -0500

    developers.

---
+++ src/components/Developers/Developer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Developer = (props) => {
+    return (
+        <h2>Developer</h2>
+
+    )
+
+}
+export default Developer;
---
+++ src/components/Developers/Developers.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Developers = (props) => {
+    return (
+        <h2>Developers</h2>
+
+    )
+
+}
+export default Developers;
